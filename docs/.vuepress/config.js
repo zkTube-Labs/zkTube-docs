059d3257c1756396805bfe743ef17f9c363c603b
damonzktube <88880642+damonzktube@users.noreply.github.com>
2021-08-13 20:37:47 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -18,6 +18,7 @@ module.exports = {
                       '/faq/What is the donation link',
                       '/faq/Do we have official WeChat group',
                       '/faq/Where do I get my test coin',
+                      '/faq/The balance problem which the deposit amount doesn’t show up means we cannot withdraw',
               ],
               '/': [
                   '/Introduction',      

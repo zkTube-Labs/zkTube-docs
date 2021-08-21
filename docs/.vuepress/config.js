23e9063e7e4556eef30b71567f9d9ec39c476b49
damonzktube <88880642+damonzktube@users.noreply.github.com>
2021-08-21 15:31:45 +0800

    Update config.js

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -16,6 +16,10 @@ module.exports = {
             //     text:'Tutorials',
             //     link:'/Tutorials/'
             // },
+            // {
+            //     text:'PayTubeDocs',
+            //     link:'/PayTubeDocs/'
+            // },
             {
                 text:'FAQ',
                 link:'/faq/'
@@ -30,6 +34,10 @@ module.exports = {
             },
         ],
         sidebar:{
+              '/PayTubeDocs/':[
+                '/PayTubeDocs/',
+                '/PayTubeDocs/PRIVACY POLICY',
+              ],
              '/Whitepaper/':[
                 '/Whitepaper/',
               ],
